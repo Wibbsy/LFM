ee7f6cd03b685c01afc8edf39e410f4630b612d3
Jaron Bradley <jaron.bradley@ge.com>
2013-09-06 17:03:57 -0400

    updated level1

--- level1.py
+++ level1.py
@@ -2,24 +2,27 @@ print "\n\nWelcome to King of the Flesh Munchers"
 
 waiting = True
 while waiting:
-  print "What is your race?\n1.Human\n2.Dworf\n3.Elf\n4.Black Guy"
-  choice = int(raw_input())
-  if choice == 1:
-      playerRace = "Human"
-      waiting = False
-  elif choice == 2:
-      playerRace = "Dworf"
-      waiting = False
-  elif choice == 3:
-      playerRace = "Elf"
-      waiting = False
-  elif choice == 4:
-      print "No you're not\n"
-  else:
-      print "That is not an option"
+    print "What is your race?\n1.Human\n2.Dworf\n3.Elf\n4.Black Guy"
+    choice = int(raw_input())
+    if choice == 1:
+        playerRace = "Human"
+        waiting = False
+        print "I thought you smelled kind of funny"
+    elif choice == 2:
+        playerRace = "Dworf"
+        print "Ah, a majesticly short, fat, bearded  clan. Good choice."
+        waiting = False
+    elif choice == 3:
+        playerRace = "Elf"
+        print "You hide very pointy ears behinid those flowing locks of yours."
+        waiting = False
+    elif choice == 4:
+        print "No you're not\n"
+    else:
+        print "That is not an option"
     
-print "\n\nI see..and your name? "
+print "\n\nand your name...? "
 playerName = raw_input()
 
-print "\n\nThat is the worst %s name i've ever heard" % (playerRace)
+print "\n\nThat is the worst %s name i've ever heard, but okay..." % (playerRace)
 print "You are Lord %s. Warrior for the %s race" % (playerName, playerRace)
